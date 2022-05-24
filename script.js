b680b0bb4a51e98b0681d9d228abe494a90bdef4
Philipe Almeida <devpalmeida@gmail.com>
2022-05-24 01:54:21 +0100

    update content and style

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const DESK_CONTENT =
-    `Hi. I’m Philipe,<br>a Front End & UI<br>Designer, focused in architect user<br>interface<br>
-    <span class="sn">——</span> currently<br> working at <span class="sight">capgemini.</span>`;
+    `Hi. I’m Philipe.<br>A software engineer,<br>focused in user interfaces.<br>
+    <span class="sn">——</span> currently working at <span class="sight">capgemini.</span>`;
 
 const MOB_CONTENT =
-    `Hi. I’m Philipe,<br>a Front End<br>& UI Designer, focused in architect user interface<br>
-    <span class="sn">——</span> currently<br> working at <span class="sight">capgemini.</span>`;
+    `Hi. I’m Philipe.<br>A software engineer,<br> focused in<br> user interfaces.<br>
+    <span class="sn">——</span> currently working at <span class="sight">capgemini.</span>`;
 
 const $HERO = document.getElementById('main-content');
 
-const contentUpdate = (() => {
+const contentUpdate = () => {
+    window.addEventListener('resize', contentUpdate);
     const setContent = isMobile => isMobile ? $HERO.innerHTML = MOB_CONTENT : $HERO.innerHTML = DESK_CONTENT;
     document.documentElement.style.setProperty('--win-height', `${window.innerHeight}px`);
     (window.matchMedia("(max-width: 500px)").matches) ? setContent(true) : setContent();
-})();
+};
 
-window.addEventListener('resize', contentUpdate);
+window.addEventListener('load', contentUpdate);
