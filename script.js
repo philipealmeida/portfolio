4dd26511a927cf48a50b324bd680b1f101a2b968
Philipe Almeida <devpalmeida@gmail.com>
2023-07-13 18:24:07 +0100

    update style in system light theme pref, and refactor js scope

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const DESK_CONTENT =
-    `Hi. I’m Philipe.<br>A software engineer,<br>focused in user interfaces.<br>
-    <span class="sn">——</span> currently working at <span class="sight">diconium.</span>`;
-
-const MOB_CONTENT =
-    `Hi. I’m Philipe.<br>A software engineer,<br> focused in<br> user interfaces.<br>
-    <span class="sn">——</span> currently working at <span class="sight">diconium.</span>`;
-
+const DESK_CONTENT = `Hi. I’m Philipe.<br>A software engineer,<br>focused in user interfaces.<br>
+    <span class='sn'>——</span> currently working at <span class='sight'>diconium.</span>`;
+const MOB_CONTENT = `Hi. I’m Philipe.<br>A software engineer,<br> focused in<br> user interfaces.<br>
+    <span class='sn'>——</span> currently working at <span class='sight'>diconium.</span>`;
 const $HERO = document.getElementById('main-content');
 
 const contentUpdate = () => {
-    window.addEventListener('resize', contentUpdate);
-    const setContent = isMobile => isMobile ? $HERO.innerHTML = MOB_CONTENT : $HERO.innerHTML = DESK_CONTENT;
-    document.documentElement.style.setProperty('--win-height', `${window.innerHeight}px`);
-    (window.matchMedia("(max-width: 500px)").matches) ? setContent(true) : setContent();
+    const setContent = (isMobile) => {
+        $HERO.innerHTML = isMobile ? MOB_CONTENT : DESK_CONTENT;
+    };
+
+    const handleResize = () => {
+        document.documentElement.style.setProperty(
+            '--win-height',
+            `${window.innerHeight}px`
+        );
+        window.matchMedia('(max-width: 500px)').matches
+            ? setContent(true)
+            : setContent(false);
+    };
+
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('load', handleResize);
 };
 
-window.addEventListener('load', contentUpdate);
+contentUpdate();
